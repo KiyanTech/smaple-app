31ccb70b6cbd1109719ec08b5c034109ff259b6d
Susanta Das <babaitech01@gmail.com>
2024-02-26 02:18:39 +0530

    new commit

--- src/main/java/com/smaple/MathSample.java
+++ src/main/java/com/smaple/MathSample.java
@@ -8,10 +8,16 @@ public class MathSample {
 		
 		return a+b;
 	}
+	public static int sub(int a,int b) {
+		
+		return a-b;
+	}
 	public static void main(String ...arg) {
 		
 		System.out.println("Hi");
 		System.out.println("Hi " + sum(10,20)) ;
+		System.out.println("Hi " + sub(10,20)) ;
+		
 	}
 	
 }
