9267cddd6511c4d6882fcf039386ef7b5c3eb9a4
Susanta Das <babaitech01@gmail.com>
2024-02-26 01:36:42 +0530

    updated MathSample.java added

--- src/main/java/com/smaple/MathSample.java
+++ src/main/java/com/smaple/MathSample.java
@@ -4,9 +4,14 @@ package com.smaple;
 public class MathSample {
 	
 	
+	public static int sum(int a,int b) {
+		
+		return a+b;
+	}
 	public static void main(String ...arg) {
 		
 		System.out.println("Hi");
+		System.out.println("Hi " + sum(10,20)) ;
 	}
 	
 }
